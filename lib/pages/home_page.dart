6dabc0b17b94de62c72b1efca3734dfdc5c509ef
vijay <vijaykumar.balabascarin3@etu.univ-lorraine.fr>
2023-02-01 18:18:51 +0100

    added bottom navbar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,18 +1,29 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   HomePage({super.key});
 
-  final user = FirebaseAuth.instance.currentUser!;
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
 
   // sign user out method
   void signUserOut() {
     FirebaseAuth.instance.signOut();
   }
 
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    final user = FirebaseAuth.instance.currentUser!;
     return Scaffold(
       backgroundColor: Colors.grey[300],
       appBar: AppBar(
@@ -29,6 +40,34 @@ class HomePage extends StatelessWidget {
         "Connecté en tant que: " + user.email!,
         style: TextStyle(fontSize: 20),
       )),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.route),
+            label: 'Itinéraire',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: 'Profil',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map),
+            label: 'Carte',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            label: 'Carnet de voyage',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.eco_outlined),
+            label: 'Suivi CO2',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Color.fromARGB(255, 100, 143, 122),
+        unselectedItemColor: Colors.black,
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
